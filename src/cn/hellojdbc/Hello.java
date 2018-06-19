be71db703990d5eb189a1982f5c164960d6374a9
sunsent <sentsent63@gmail.com>
2018-06-19 23:27:10 +0800

    1

--- src/cn/hellojdbc/Hello.java
+++ src/cn/hellojdbc/Hello.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Hello {
     @Test
-    public void fun1() throws Exception{
+    public void insert() throws Exception{
         //1. jdbc注册驱动
         DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
         //2. 连接数据库
@@ -26,6 +27,30 @@ public class Hello {
                 "18);\n";
         statement.executeUpdate(sql);
 
+        //4. 关闭连接
+        statement.close();
+        conn.close();
+    }
+    @Test
+    public void query() throws Exception{
+        //1. jdbc注册驱动
+        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        //2. 连接数据库
+        /*
+        ?useUnicode=true&characterEncoding=UTF-8，这个是解决中文乱码输入问题
+        UTC是统一标准世界时间
+         */
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/day05?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC", "root", "1234");
+        //3. 操作数据库
+        Statement statement=conn.createStatement();
+        String sql="select * from `day05`.`t_user`;";
+        ResultSet resultSet=statement.executeQuery(sql);
+        while (resultSet.next()){
+            int id= resultSet.getInt("id");
+            String name=resultSet.getString("name");
+            int age=resultSet.getInt("age");
+            System.out.println(id+name+age);
+        }
         //4. 关闭连接
         statement.close();
         conn.close();
